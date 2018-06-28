1a3374a3347afc9ccd0be58457751d3c3ed3b875
Hyukho Kwon <hyukhok@sfu.ca>
2018-06-27 20:58:06 -0700

    Add items on Menu
    Add four linear layouts on MainMenu Activity

---
+++ app/src/main/java/com/example/walkingschoolbus/MainMenu.java
@@ -0,0 +1,36 @@
+package com.example.walkingschoolbus;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main_menu );
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle( R.string.main_menu );
+
+
+
+    }
+
+
+
+    //Make a intent to move to this activity
+    public Intent makeIntent(Context context){
+        Intent intent = new Intent( context, MainMenu.class );
+        return intent;
+
+    }
+
+
+
+
+}
