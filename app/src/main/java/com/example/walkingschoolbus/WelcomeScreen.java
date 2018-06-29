dfdef522cd510deefeb3a4d1e9c05ea27579e9da
bhaley <bhaley@sfu.ca>
2018-06-28 22:38:13 -0700

    bypass login screen
    Create button to  bypass login screen.

--- app/src/main/java/com/example/walkingschoolbus/WelcomeScreen.java
+++ app/src/main/java/com/example/walkingschoolbus/WelcomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.walkingschoolbus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        setupDebugButton();
+    }
+//TODO Remove this before submission
+    private void setupDebugButton() {
+        Button btn = (Button) findViewById(R.id.buttonDebug);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainMenu.makeIntent(WelcomeScreen.this);
+                startActivity(intent);
+            }
+        });
     }
 }
