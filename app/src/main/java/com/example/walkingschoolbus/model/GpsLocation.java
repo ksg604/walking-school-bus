4a2669df07d455b26f3d56e0db0d9d9963b5f8c3
shixiny <shixiny@sfu.ca>
2018-06-30 23:11:22 -0700

    Add log-in & sing-up function
    User could log-in with existing account or create a new on. After logging-in, user will be directed to the main page.

---
+++ app/src/main/java/com/example/walkingschoolbus/model/GpsLocation.java
@@ -0,0 +1,11 @@
+package com.example.walkingschoolbus.model;
+
+import android.location.Location;
+
+public class GpsLocation {
+    private Location location;
+    private double longtitude;
+    private double lantitude;
+
+
+}
