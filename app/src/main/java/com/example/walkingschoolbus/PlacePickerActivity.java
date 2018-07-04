2d9b4b3a576fef499b31b1e4a4d2e3bed4df9a6f
bhaley <bhaley@sfu.ca>
2018-07-04 12:10:50 -0700

    Finish Layout
    Complete layout and activity for place picker. Do not implement posting new group object.

--- app/src/main/java/com/example/walkingschoolbus/PlacePickerActivity.java
+++ app/src/main/java/com/example/walkingschoolbus/PlacePickerActivity.java
@@ -2,21 +2,28 @@ package com.example.walkingschoolbus;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 
 public class PlacePickerActivity extends AppCompatActivity {
 
-    int PLACE_PICKER_REQUEST = 1;
+    int PLACE_PICKER_LOC_REQUEST = 1;
+    int PLACE_PICKER_MEET_REQUEST =2;
     private final static String TAG = "Place Picker Activity";
+    private LatLng primaryLocation;
+    private LatLng meetupLocation;
+
 
 
     @Override
@@ -32,7 +39,7 @@ public class PlacePickerActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                goPlacePicker();
+                goPlacePicker(PLACE_PICKER_LOC_REQUEST);
             }
         });
     }
@@ -42,16 +49,16 @@ public class PlacePickerActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goPlacePicker();
+                goPlacePicker(PLACE_PICKER_MEET_REQUEST);
             }
         });
     }
 
     //Source https://www.youtube.com/channel/UCYN1_QmpaIGZNiwGSzFbE2w
-    public void goPlacePicker(){
+    public void goPlacePicker(int reqCode){
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
         try{
-            startActivityForResult(builder.build(PlacePickerActivity.this),PLACE_PICKER_REQUEST);
+            startActivityForResult(builder.build(PlacePickerActivity.this),reqCode);
             Log.i(TAG,"Start Place Picker");
         }catch(GooglePlayServicesRepairableException e){
             e.printStackTrace();
@@ -65,9 +72,19 @@ public class PlacePickerActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         Log.i(TAG,"result code: " +Integer.toString(resultCode));
-        if(requestCode ==PLACE_PICKER_REQUEST){
-            if(requestCode== RESULT_OK){
+        TextView location = (TextView) findViewById(R.id.txtViewPrimaryLoc);
+        TextView meetupLoc = (TextView) findViewById(R.id.txtViewMeetupLoc);
+        if(requestCode == PLACE_PICKER_LOC_REQUEST ||requestCode == PLACE_PICKER_MEET_REQUEST){
+            if(resultCode== RESULT_OK){
                 Place place = PlacePicker.getPlace(PlacePickerActivity.this, data);
+                if(requestCode ==PLACE_PICKER_MEET_REQUEST) {
+                    location.setText(place.getAddress());
+                    primaryLocation = place.getLatLng();
+                } else{
+                    meetupLoc.setText(place.getAddress());
+                    meetupLocation = place.getLatLng();
+                }
+
             }
         }
     }
