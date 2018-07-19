c90c051ad911a1d905939226acfc0dc6fac09a05
Kevin San Gabriel <kevinsan@uvic.ca>
2018-07-18 19:23:55 -0700

    Added parents dashboard activity

---
+++ app/src/main/java/com/example/walkingschoolbus/ParentsDashboardActivity.java
@@ -0,0 +1,262 @@
+package com.example.walkingschoolbus;
+
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import android.location.LocationProvider;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.walkingschoolbus.model.GpsLocation;
+import com.example.walkingschoolbus.model.Group;
+import com.example.walkingschoolbus.model.Session;
+import com.example.walkingschoolbus.model.User;
+import com.example.walkingschoolbus.proxy.ProxyBuilder;
+import com.example.walkingschoolbus.proxy.WGServerProxy;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import retrofit2.Call;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+
+/**
+ * Simple test app to show a Google Map.
+ * - If using the emulator, Create an Emulator from the API 26 image.
+ *   (API27's doesn't/didn't support maps; nor will 24 or before I believe).
+ * - Accessing Google Maps requires an API key: You can request one for free (and should!)
+ *   see /res/values/google_maps_api.xml
+ * - More notes at the end of this file.
+ */
+
+public class ParentsDashboardActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static GpsLocation gpsLocation;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Boolean mLocationPermissionsGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 0;
+    private static final float DEFAULT_ZOOM = 14f;
+    WGServerProxy proxy;
+    private Session token = Session.getInstance();
+
+    private Marker kidLocationMarker;
+    private User currentUser;
+
+    private Map<Marker, Long> markerLongHashMapMap = new HashMap<Marker, Long>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        currentUser = Session.getInstance().getUser();
+
+        String tokenValue= token.getToken();
+
+        proxy = ProxyBuilder.getProxy(getString(R.string.api_key),tokenValue);
+        getUserLocationPermission();
+
+    }
+
+
+    private void getAllKids(){
+        Call<List<User>> kidListCaller = proxy.getMonitorsUsers(currentUser.getId());
+        ProxyBuilder.callProxy(ParentsDashboardActivity.this, kidListCaller, returnedKids -> response(returnedKids));
+    }
+    private void response(List<User> currentUserKids){
+        for(User myKid: currentUserKids){
+            kidLocationMarker = mMap.addMarker(new MarkerOptions()
+                    .position( myKid.getLastGpsLocation() )
+                    .title( "Group: " + group.getGroupDescription() ));
+        }
+    }
+
+    private void initMap(){
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        getAllKids();
+
+        //If user enables the app to access their location, get user location.
+        if(mLocationPermissionsGranted){
+            getUserLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Long groupId = markerLongHashMapMap.get(marker);
+
+                Intent intent = OnMarkerClickActivity.makeIntent(getApplicationContext());
+                intent.putExtra("id",groupId);
+                startActivity(intent);
+                //setContentView(R.layout.activity_on_marker_click);
+                return false;
+            }
+        });
+
+    }
+
+    /**
+     * Retrieves the phones location and marks it on the map.
+     * The map will automatically centre on to the phone location upon initiation of the map.
+     * There is a button on the top right corner of the map that will centre on to the phones's location if clicked.
+     */
+    //Source: https://www.youtube.com/watch?v=fPFr0So1LmI&list=PLgCYzUzKIBE-vInwQhGSdnbyJ62nixHCt&index=5
+    private void getUserLocation(){
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            Task location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()) {
+                        Location currentLocation = (Location) task.getResult();
+                        if (currentLocation == null) {
+                            Location sfu = new Location("");
+                            sfu.setLatitude(49.27);
+                            sfu.setLongitude(-122.98);
+                            moveCamera(new LatLng(sfu.getLatitude(), sfu.getLongitude()), DEFAULT_ZOOM);
+                        } else{
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                        }
+                    }
+                }
+            });
+        }catch (SecurityException exception){
+            Log.e("MapsActivity","Security Exception: "+exception.getMessage());
+        }
+    }
+
+    /**
+     * Centers the screen to the specified position.
+     * @param latLng Position coordinates of the location
+     * @param zoom Zooms on the location by a specified amount.
+     */
+    private void moveCamera(LatLng latLng,float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+    }
+
+    /**
+     * Requests permission from the user to allow location services for the map.
+     */
+    //Source: https://www.youtube.com/watch?v=Vt6H9TOmsuo&index=4&list=PLgCYzUzKIBE-vInwQhGSdnbyJ62nixHCt
+    private void getUserLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        //If both ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION permissions are granted,
+        //device location permissions will be granted.
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mLocationPermissionsGranted = true;
+            initMap();
+        }else{
+            //Else, request the user to enable location services for the map.
+            //LOCATION_PERMISSION_REQUEST_CODE will be passed to onRequestPermissionsResult method
+            //to verify the results of user's selection.
+            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+
+    }
+
+    /**
+     *
+     * @param requestCode The code passed by getUserLocationMethod providing the results of whether user denied or allowed location services.
+     * @param permissions The array of requested permissions.
+     * @param grantResults Grant results for the requested permissions.  Either PERMISSION_GRANTED or PERMISSION_DENIED.
+     */ //Source: https://www.youtube.com/watch?v=Vt6H9TOmsuo&index=4&list=PLgCYzUzKIBE-vInwQhGSdnbyJ62nixHCt
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionsGranted = false;
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionsGranted = true;
+                }
+        }
+    }
+
+
+
+    public static Intent makeIntent(Context context){
+        Intent intent = new Intent(context, ParentsDashboardActivity.class);
+        return intent;
+    }
+}
+
+
+
